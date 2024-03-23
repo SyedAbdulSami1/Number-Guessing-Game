0cb694828009beb397bbc8203805179d23c5b6d3
SyedAbdulSami1 <samiwpp@gmail.com>
2024-03-24 03:14:53 +0500

    for updated version

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 #! /usr/bin/evn node
 import inquirer from "inquirer";
 const computerGuessNumber = Math.floor(Math.random() * 6 + 1);
-const answer = await inquirer.prompt([{
+const answer = await inquirer.prompt([
+    {
         name: "PlayerNumber",
         type: "number",
         message: "Please Enter Your Guessing Number between 1 t0 6: ",
-    }]);
+    },
+]);
 if (computerGuessNumber === answer.PlayerNumber) {
     console.log(`Congratulation! you guessed right number.`);
 }
--- index.ts
+++ index.ts
@@ -2,15 +2,19 @@
 
 import inquirer from "inquirer";
 
-const computerGuessNumber : number = Math.floor(Math.random()*6+1);
-const answer = await inquirer.prompt ([{
+const computerGuessNumber: number = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+  {
     name: "PlayerNumber",
     type: "number",
     message: "Please Enter Your Guessing Number between 1 t0 6: ",
-}]);
+  },
+]);
 
-if (computerGuessNumber===answer.PlayerNumber) {
-    console.log(`Congratulation! you guessed right number.`);
+if (computerGuessNumber === answer.PlayerNumber) {
+  console.log(`Congratulation! you guessed right number.`);
 } else {
-    console.log(`Your given number is Wrong, correct number is ${computerGuessNumber}`);
+  console.log(
+    `Your given number is Wrong, correct number is ${computerGuessNumber}`
+  );
 }
