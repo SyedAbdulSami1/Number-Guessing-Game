ce6ef3985a74e469b5a9a49ec977103f40a5fb15
SyedAbdulSami1 <samiwpp@gmail.com>
2024-03-24 02:40:33 +0500

    Shabang add for publishing

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+#! /usr/bin/evn node
 import inquirer from "inquirer";
-const computerGuessNumber = 5;
+const computerGuessNumber = Math.floor(Math.random() * 6 + 1);
 const answer = await inquirer.prompt([{
         name: "PlayerNumber",
         type: "number",
-        message: "Please Enter Your Guessing Number",
+        message: "Please Enter Your Guessing Number between 1 t0 6: ",
     }]);
 if (computerGuessNumber === answer.PlayerNumber) {
-    console.log(`Congratulation You Win`);
+    console.log(`Congratulation! you guessed right number.`);
 }
 else {
     console.log(`Your given number is Wrong, correct number is ${computerGuessNumber}`);
--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-
+#! /usr/bin/evn node
 
 import inquirer from "inquirer";
 
-const computerGuessNumber : number = Math.random();
+const computerGuessNumber : number = Math.floor(Math.random()*6+1);
 const answer = await inquirer.prompt ([{
     name: "PlayerNumber",
     type: "number",
-    message: "Please Enter Your Guessing Number",
+    message: "Please Enter Your Guessing Number between 1 t0 6: ",
 }]);
 
 if (computerGuessNumber===answer.PlayerNumber) {
-    console.log(`Congratulation You Win`);
+    console.log(`Congratulation! you guessed right number.`);
 } else {
     console.log(`Your given number is Wrong, correct number is ${computerGuessNumber}`);
 }
